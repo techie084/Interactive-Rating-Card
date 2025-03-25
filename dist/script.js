50d11fbf9344cd2573b7c0e4c4b0dcbaba498454
Techie 🤸️🤍️ <wisdommatthew08@gmail.com>
2025-03-25 16:39:04 +0100

    changes

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,5 @@
 // Always use strict mode
 "use strict";
-
 ///////////////////////////////////////////////////////////////
 // Declaring Variables
 const btnSubmit = document.querySelector(".btn");
@@ -11,44 +10,40 @@ const selected = document.querySelector(".selected");
 const btnCloseModal = document.querySelector(".close-modal");
 const cardContainer = document.querySelector(".card-container");
 const errorMsg = document.querySelector(".error-msg");
-let buttonText: string = "";
-
+let buttonText = "";
 // Style the Selected number
-numbersBtn.forEach((number: any) => {
-  number.addEventListener("click", () => {
-    errorMsg?.classList.add("hidden");
-    number.classList.add("active");
-    buttonText = number.textContent;
-  });
+numbersBtn.forEach((number) => {
+    number.addEventListener("click", () => {
+        errorMsg?.classList.add("hidden");
+        number.classList.add("active");
+        buttonText = number.textContent;
+    });
 });
-
 // Function for opening the Modal
 btnSubmit?.addEventListener("click", () => {
-  if (buttonText) {
-    selected.textContent = buttonText;
-    modal?.classList.remove("hidden");
-    overlay?.classList.remove("hidden");
-  } else {
-    errorMsg?.classList.remove("hidden");
-  }
+    if (buttonText) {
+        selected.textContent = buttonText;
+        modal?.classList.remove("hidden");
+        overlay?.classList.remove("hidden");
+    }
+    else {
+        errorMsg?.classList.remove("hidden");
+    }
 });
-
 // Function for Closing the modal
 const closeModal = () => {
-  modal?.classList.add("hidden");
-  overlay?.classList.add("hidden");
-  numbersBtn.forEach((numberBtn) => {
-    numberBtn.classList.remove("active");
-  });
+    modal?.classList.add("hidden");
+    overlay?.classList.add("hidden");
+    numbersBtn.forEach((numberBtn) => {
+        numberBtn.classList.remove("active");
+    });
 };
-
 btnCloseModal?.addEventListener("click", () => {
-  closeModal();
+    closeModal();
 });
-
 // Closing the modal with escape key
 document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape" && !modal?.classList.contains("hidden")) {
-    closeModal();
-  }
+    if (e.key === "Escape" && !modal?.classList.contains("hidden")) {
+        closeModal();
+    }
 });
